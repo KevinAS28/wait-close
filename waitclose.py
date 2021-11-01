1a1789ff24831db3b490bacb3bcc26b3fccc4315
KevinAS28 <kevinagusto28@gmail.com>
2021-11-01 07:40:17 +0700

    kill command py

--- waitclose.py
+++ waitclose.py
@@ -1,12 +1,16 @@
 import os, time
 
+kill_command = 'killall zoom'
+print(kill_command)
+
 def kill():
-    os.system('killall zoom')
+    os.system(kill_command)
 wait_time = 60*60 + 15*60
+
 
 for i in range(int(wait_time)):
     print(i, wait_time)
     time.sleep(1)
-    
+
 kill()
 print('Killed at: ', time.ctime())
